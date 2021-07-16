16271fd99f958e38021ec01e245c9fe039388db6
Christian Dressler <christian.dressler@chemie.uni-halle.de>
2021-07-16 14:07:09 +0200

    second

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
+import os
 from setuptools import setup
 import subprocess
 
+
+from numpy.distutils.core import setup, Extension
 
 def get_commit_hash():
         command = "git log -n 1 --format=%H%n%s%n%ad"
@@ -11,12 +14,40 @@ def get_commit_hash():
         return commit_hash.decode(), commit_message.decode(), commit_date.decode()
 
 
+def find_packages():
+    """Find all packages (i.e. folders with an __init__.py file inside)"""
+    packages = []
+    for root, _, files in os.walk("mes_from_cpmd"):
+        for f in files:
+            if f == "__init__.py":
+                packages.append(root)
+    return packages
+
+
+def readme():
+    with open('README.rst', 'r') as f:
+        return f.read()
+
+
+
+print(find_packages())
+
+
+
+ext1 = Extension(name='mes_from_cpmd.ext_fortran.fortran_io',
+                 sources=['mes_from_cpmd/ext_fortran/fortran_io.F90'],
+                 f2py_options=['--quiet'],
+                )
+
 
 
 setup(name='mes_from_cpmd',
      version='19.1',
+     #packages=['mes_from_cpmd'],
+     packages=find_packages() + ['commit_stuff'],
      #description='Markov models for lithium transfer',
      #author='Christian Dreßler',
+     long_description=readme(),
                                entry_points={
                                     'console_scripts': [
                                         #"jump_mat_ana = ana_tools.jump_mat_analyzer:main",
@@ -35,13 +66,19 @@ setup(name='mes_from_cpmd',
                                         #"reduce_trajec = ascripts.reduce_trajec:main",
                                         #"reduce_trajec = bscripts.reduce_trajec:main",
                                         #"prepare_trajec = ascripts.prepare_trajec:main"]
-                                        "first_try = mes_from_cpmd.1:main"]
+                                        #"first_try = MES_from_cpmd.mes_from_cpmd.1:main"]
+                                        #"first_try = mes_from_cpmd.1:main",
+                                        "gen_pot = mes_from_cpmd.pot.gen_pot:main",
+                                        "cal_dens_for_pot = mes_from_cpmd.cal_dens_for_pot:main",
+                                        "cal_cube_from_wan_for_dens = mes_from_cpmd.cal_cube_from_wan_for_dens:main",
+                                        #"second_try = mes_from_cpmd.2:main"]
                                                                },
                                     #zip_safe=False)
 include_package_data=True,
       zip_safe=False,
-      #ext_modules=ext_modules,
-      #cmdclass={'build_ext': build_ext}
+       ext_modules=[ext1],
+#      ext_modules=ext_modules,
+#      cmdclass={'build_ext': build_ext}
                                     
 )
 
