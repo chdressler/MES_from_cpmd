3221a4181d191138e47e14c7cca70e93adfd97d0
Till Hanke <hanke@ip-141-48-225-227>
2021-10-18 11:21:21 +0200

    hanke transfer

--- commit_stuff/version_hash.py
+++ commit_stuff/version_hash.py
@@ -1,3 +1,3 @@
-commit_hash = "6ec43a90202c365ad087585e9799745e50b926d7"
-commit_message = " changes"
-commit_date = "Fri Jul 16 15:25:14 2021 +0200"
+commit_hash = "41fcf5fbc96dedd898075383fbea4b185ea6c03c"
+commit_message = " ready for github"
+commit_date = "Wed Jul 21 10:50:10 2021 +0200"
